eae663aa5a61198175fbbec57f35d7c57155d018
Milind Murhekar <30992844+mmurhekar@users.noreply.github.com>
2018-12-24 16:47:41 +0530

    MergeSort

---
+++ MergeSort.c
@@ -0,0 +1,89 @@
+/*
+ * MergeSort
+ * Divide and Conquer method */
+
+#include <stdio.h>
+#define MAX_ELE 20
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*
+ * arr = 34 5 0 2 1 10 3 */
+
+void merge(int *arr, int l, int m, int r)
+{
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int lt[100], rt[100], i, j, k;
+
+	for (i = 0; i < n1; i++)
+		lt[i] = arr[l + i];
+
+	for (j = 0; j < n2; j++)
+		rt[j] = arr[m + 1 + j];
+
+	i = 0;
+	j = 0;
+	k = l;
+
+	while (i < n1 && j < n2) {
+		if (lt[i] <= rt[j]) {
+			arr[k] = lt[i];
+			i++;
+		} else {
+			arr[k] = rt[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+			arr[k] = lt[i];
+			i++;
+			k++;
+	}
+
+	while (j < n2) {
+			arr[k] = rt[j];
+			j++;
+			k++;
+	}
+}
+
+void mergesort(int *arr, int l, int r)
+{
+	int mid;
+	if (l < r) {
+		mid = l + (r - l)/2;
+		mergesort(arr, l, mid);
+		mergesort(arr, mid + 1, r);
+		merge(arr, l, mid, r);
+	}
+}
+
+int main ()
+{
+	int array[] = {-2, 0, -9, 45, 11, 4, 10};
+	int n = sizeof(array)/sizeof(array[0]), i;
+
+	printf("\n");
+
+	for (i = 0; i < n; i++)
+		printf("%d ", array[i]);
+
+	printf("\n");
+
+	mergesort(array, 0, n-1);
+
+	for (i = 0; i < n; i++)
+		printf("%d ", array[i]);
+
+	printf("\n");
+
+	return 0;
+}
